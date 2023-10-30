3fa217a56d49ad7be2c3b15462eb9e617ffe1f90
Van Dung Hoang <hoangvandung0210@gmail.com>
2023-10-30 10:29:51 +0700

    ver1.0

---
+++ app/src/main/java/com/example/do_an_di_dong_giua_ky_1/Activity_MuaHang.java
@@ -0,0 +1,14 @@
+package com.example.do_an_di_dong_giua_ky_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_MuaHang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muahang);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "do_an_di_dong_giua_ky_1"
+include(":app")
